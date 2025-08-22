1a98cc1ac10dd9c3efbfa0249e236b91e41d5e99
David Hedding <dghedding@gmail.com>
2025-08-22 10:05:10 -0600

    updated products.py

--- CRUD/products.py
+++ CRUD/products.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request
-import json
+from flask import Flask, jsonify, request 
+import json 
 from flask_cors import CORS
-
 app = Flask("Product Server")
 CORS(app)
 
@@ -11,14 +10,25 @@ products = [
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
 
-@app.route("/products", method="["GET"])
-def get_products():
+@app.route("/products/<id>", methods=["GET", "PUT", "DELETE"])
+def product_methods(id):
     id = int(id)
     product = [x for x in products if x["id"] == id][0]
-    return jsonify(products)
-
-
-
-
-
+    if request.method == "GET":
+    	return jsonify(product)
+    if request.method == "PUT":
+        updated_product = json.loads(request.data)
+        for key, value in updated_product.items():
+            product[key] = value
+        return '', 204
+    if request.method == "DELETE":
+        products.remove(product)
+        return '', 204
+@app.route("/products", methods=["GET", "POST"])
+def products_methods(): 
+    if request.method == "GET":
+        return jsonify(products)
+    if request.method == "POST":
+        products.append(request.get_json())
+        return '', 201
 app.run(port=5000,debug=True)
